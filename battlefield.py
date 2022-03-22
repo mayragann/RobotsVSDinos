771867145d949e8978fea7b24233f3117a6ab6ef
mayragann <mayragann@gmail.com>
2022-03-22 11:52:50 -0400

    completed the run game function as well as the display welcome message

--- battlefield.py
+++ battlefield.py
@@ -2,6 +2,7 @@ from dinosaur import Dinosaur
 from robots import Robot
 from fleet import Fleet
 from herd import Herd
+import random
 
 class Battlefield:
     def __init__(self) -> None:
@@ -10,9 +11,21 @@ class Battlefield:
         
     def run_game(self):
         self.display_welcome()
-        )
+        self.team = self.choose_team()
+        self.battle()
+
+
     def display_welcome(self):
-        pass
+        print("Welcome to the brawl of the century!")
+        print("Are you ready to rummmmbleeeeee?")
+        print("-------------------------------------")
+        print("Dinosaurs versus Robots!")
+        print("Both Dinosaurs and Robots start with 200 health")
+        print("Each contestant begins with 150 stamina")
+        print("Attacks will drain each contestant of 10")
+        print("There is a fleet of robots and a herd of dinosaurs")
+        print("A winner is declared once all Robots in fleet")
+        print("or all Dinosaurs in a herd have 0 health")
     def battle(self):
         pass
     def dino_turn(self, dinosaur):
